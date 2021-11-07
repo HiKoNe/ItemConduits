4c96569408d5f4dd2c7eb941c11336afcc8367cb
Костя <necrasow.kostja@yandex.ru>
2021-11-07 13:46:26 +0300

    0.1.3.0

--- Contents/ItemItemConduit.cs
+++ Contents/ItemItemConduit.cs
@@ -1,9 +1,15 @@
 ﻿using ConduitLib.APIs;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
 
 namespace ItemConduits.Contents
 {
     public class ItemItemConduit : AbstractItemConduit<ItemConduit>
     {
+        public override void SetStaticDefaults() =>
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 50;
 
+        public override void ModifyResearchSorting(ref ContentSamples.CreativeHelper.ItemGroup itemGroup) =>
+            itemGroup = ContentSamples.CreativeHelper.ItemGroup.Material;
     }
 }
